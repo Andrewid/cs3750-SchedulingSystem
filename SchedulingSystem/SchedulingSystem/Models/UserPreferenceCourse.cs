416aae95a67eb77424ac8a8605a4bc866b8ab28d
Chase Greenwood <chasewgreenwood@gmail.com>
2018-07-16 08:48:59 -0600

    Altered tables to match updated Database Schema, added DemandSource table

--- SchedulingSystem/SchedulingSystem/Models/UserPreferenceCourse.cs
+++ SchedulingSystem/SchedulingSystem/Models/UserPreferenceCourse.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 
 namespace SchedulingSystem.Models
 {
-    public class InstructorPreferenceCourse
+    public class UserPreferenceCourse
     {
-        public int InstructorPreferenceCourseID { get; set; }
-        public int InstructorID { get; set; }
+        public int UserPreferenceCourseID { get; set; }
+        public int UserID { get; set; }
         public int CourseID { get; set; }
 
         public Course Course { get; set; }
