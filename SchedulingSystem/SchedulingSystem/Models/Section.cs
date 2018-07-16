1cadbdf7050c503039234730a2bf61159fe542ad
Andrewid <AndrewMerrell1@mail.weber.edu>
2018-07-16 14:42:24 -0600

    Tabbing because of OCD, sorry

--- SchedulingSystem/SchedulingSystem/Models/Section.cs
+++ SchedulingSystem/SchedulingSystem/Models/Section.cs
@@ -7,19 +7,19 @@ namespace SchedulingSystem.Models
 {
     public class Section
     {
-        public int SectionID { get; set; }
-        public int RoomID { get; set; }
-        public int CourseID { get; set; }
-        public int TimeBlockID { get; set; }
-        public int DayBlockID { get; set; }
-        public int StatusID { get; set; }
-        public int SemesterID { get; set; }
+        public int SectionID    { get; set; }
+        public int RoomID       { get; set; }
+        public int CourseID     { get; set; }
+        public int TimeBlockID  { get; set; }
+        public int DayBlockID   { get; set; }
+        public int StatusID     { get; set; }
+        public int SemesterID   { get; set; }
 
 
-         public Room Room { get; set; }  
-         public TimeBlock TimeBlock { get; set;}
-        public DayBlock DayBlock  { get; set;}
-       public Semester Semester { get; set; }
+        public Room Room            { get; set; }  
+        public TimeBlock TimeBlock  { get; set; }
+        public DayBlock DayBlock    { get; set; }
+        public Semester Semester    { get; set; }
 
         public Course Course { get; set; }
         public Status Status { get; set; }// add
